03b0ab93baceb70f8846eb548b141239281808e3
Saba-Daryabari <sabadbdb@gmail.com>
2025-11-26 20:32:19 +0330

    fixing collection styling issue.

--- src/Components/BlogPostCard.tsx
+++ src/Components/BlogPostCard.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import '../styles/blog.scss'
 
 export default function BlogPostCard({post}: {post: any}) {
--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -8,9 +8,11 @@ export default function ProductCard({ product }: Props) {
   const imageUrl = `http://localhost:3000${product.image}`;
   return (
     <div className="productCard">
+      <div className="imageContainer">
       <img src={imageUrl} className="productCardImage" />
+      </div>
       <h3 className="productCardTitle">{product.title}</h3>
-      <p className="productCardPrice">{product.price}$</p>
+      <p className="productCardPrice">${product.price}</p>
     </div>
   );
 }
